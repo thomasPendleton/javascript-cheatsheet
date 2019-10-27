be9318994de4a7af8d904e51e375f4c7131c794c
thomasPendleton <57029458+thomasPendleton@users.noreply.github.com>
2019-10-26 23:12:39 -0700

    adding rest of files

---
+++ script.js
@@ -0,0 +1,65 @@
+let todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study Javascript"
+];
+
+// for loops
+// for(var i = 0; i < todos.length; i++){
+//     console.log(todos[i]) ;
+//   };
+
+// for loop to add ! to end of every object in array
+// for(var i = 0; i < todos.length; i++){
+//    todos[i] = todos[i]+ "!"
+// };
+
+// for(var i = 0; i < todos.length; i++){
+//    console.log(todos[i]) ;
+//  };
+
+//  const todosLength = todos.length;
+//  for(var i = 0; i < todosLength; i++){
+//     todos.pop();
+//   };
+
+//   for(var i = 0; i < todos.length; i++){
+//     console.log(todos[i]) ;
+//   };
+//   console.log(todos);
+
+//forEach Loop
+// todos.forEach(function(vari, i ){
+//     console.log(vari, i );
+// })
+
+// or with a function declaration then used to callback todos.forEach(logTodos)
+
+// function logTodos(vari, i){
+//     console.log(i, vari);
+// };
+
+// todos.forEach(logTodos);
+
+
+//while loops
+
+// let counterOne = 0;
+//   while(counterOne < 10){
+//     console.log(counterOne);
+//     counterOne ++
+//   }
+//   let counterTwo = 10;
+//   while(counterTwo > 0){
+//     console.log(counterTwo);
+//     counterTwo --
+//   }
+
+// do while loop
+
+//   var counterThree = 10
+//   do{
+//       console.log(counterThree)
+//       counterThree--
+//   } while (counterThree > 0);
